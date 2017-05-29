5d9324d5b2201ba218340ffcbdf074540b067a5e
IsemanTech <johnsoni1440@gmail.com>
2017-05-28 20:42:16 -0400

    removed call to load-texture from loop. Now textures are generated once in main and texture codes are referenced through the texture-table hash table. Fixes rapid ram consumption.

--- examples/tile-map.lisp
+++ examples/tile-map.lisp
@@ -18,34 +18,32 @@ if doing tile-map with draw-tile-map"
   (with-open-file (map data-file)
     (loop for char = (read-char map nil 'eof)
        until (eq char 'eof)
-	 collect char)))
+	 nconc (list char))))
 
-(defun tile-selector (char path sprite-coord tile-pixel-coord-list path-list texture xshift yshift)
+(defun tile-selector (char path sprite-coord tile-pixel-coord-list path-list
+		      texture-table xshift yshift)
   "Select tile in list. In this ex 0 is grass and 1 is dirt"
   (if (eq char #\0)
       (progn
-	(setf texture (car path-list))
 	(setf path (car path-list))
 	(setf sprite-coord (car tile-pixel-coord-list))
-	(mariko:load-texture texture)))
+	(gl:bind-texture :texture-2d (gethash char texture-table))))
   (if (eq char #\1)
       (progn
-	(setf texture (cadr path-list))
 	(setf path (cadr path-list))
 	(setf sprite-coord (cadr tile-pixel-coord-list))
-	(mariko:load-texture texture)))
+	(gl:bind-texture :texture-2d (gethash char texture-table))))
   (unless (eq path nil)
-      (mariko:draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
-		   (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift)))
+    (mariko:draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
+		  (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift)))
 
 
-(defun draw-tiles (path-list data-file tile-pixel-coord-list x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
+(defun draw-tiles (texture-table data-file path-list tile-pixel-coord-list x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
   "read map data file and draw tiles accordingly. offset shifts the each tile in the x direction use this if tiles are not hex."
   (let ((map-list (read-file-to-list data-file)))
     (loop for char in map-list
        with path 
        with sprite-coord
-       with texture = 0
        with count = 0
        if (eq char #\NewLine)
        do (setf count (+ count 1))
@@ -53,14 +51,14 @@ if doing tile-map with draw-tile-map"
        do (setf yshift (+ yshift y-distance-apart))
        and
        do (setf xshift (* count offset))
-	 else do (setf xshift (+ xshift x-distance-apart))
-       do (tile-selector char path sprite-coord tile-pixel-coord-list path-list texture
-			 xshift yshift))))	
+       else do (setf xshift (+ xshift x-distance-apart))
+       do (tile-selector char path sprite-coord tile-pixel-coord-list path-list
+			 texture-table xshift yshift))))	
 	 
 
-(defun map-tiles (path-list coord-list)
+(defun map-tiles (path-list coord-list texture-table)
   (gl:clear :color-buffer-bit)
-  (draw-tiles path-list "sample-map.txt" coord-list 65 50 :offset 30)) 
+  (draw-tiles texture-table "sample-map.txt" path-list coord-list 65 50 :offset 30))
 			     
 (defun main ()
   (sdl2-image:init '(:png))
@@ -68,13 +66,16 @@ if doing tile-map with draw-tile-map"
     (let* ((grass "tileGrass.png")
 	   (dirt "tileDirt_full.png")
 	   (path-list (list grass dirt))
+	   (texture-table (make-hash-table))
 	   (tile-pixel-coord-list (make-sprite-list-from-singles path-list)))
-	(glfw:set-window-size-callback 'update-viewport)
-	(glfw:set-key-callback 'quit-on-escape)
-	(mariko:set-viewport 800 400)
-	(gl:clear-color 1 1 1 1)
-	(gl:clear :color-buffer)
-	(loop until (glfw:window-should-close-p)
-	   do (map-tiles path-list tile-pixel-coord-list)
-	   do (glfw:poll-events)
-	   do (glfw:swap-buffers)))))
+      (setf (gethash #\0 texture-table) (mariko:load-texture grass))
+      (setf (gethash #\1 texture-table) (mariko:load-texture dirt))
+      (glfw:set-window-size-callback 'update-viewport)
+      (glfw:set-key-callback 'quit-on-escape)
+      (mariko:set-viewport 800 400)
+      (gl:clear-color 1 1 1 1)
+      (gl:clear :color-buffer)
+      (loop until (glfw:window-should-close-p)
+	 do (map-tiles path-list tile-pixel-coord-list texture-table)
+	 do (glfw:poll-events)
+	 do (glfw:swap-buffers)))))
