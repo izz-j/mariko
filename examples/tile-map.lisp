797bc4a557040e4d874d4b6fb68502381293b58f
IsemanTech <johnsoni1440@gmail.com>
2017-05-29 06:48:33 -0400

    passes hash tables through the draw-tiles functions instead of lists. This simplifies the code in the tile-selector to just select the path and coordinates that matches the char key

--- examples/tile-map.lisp
+++ examples/tile-map.lisp
@@ -20,25 +20,20 @@ if doing tile-map with draw-tile-map"
        until (eq char 'eof)
 	 nconc (list char))))
 
-(defun tile-selector (char path sprite-coord tile-pixel-coord-list path-list
+(defun tile-selector (char path sprite-coord tile-pixel-coord-table path-table
 		      texture-table xshift yshift)
-  "Select tile in list. In this ex 0 is grass and 1 is dirt"
-  (if (eq char #\0)
+  "Select tile in according to matching char key in hash tables"
+  (unless (or (eq char #\NewLine) (eq char #\Space))
       (progn
-	(setf path (car path-list))
-	(setf sprite-coord (car tile-pixel-coord-list))
-	(gl:bind-texture :texture-2d (gethash char texture-table))))
-  (if (eq char #\1)
-      (progn
-	(setf path (cadr path-list))
-	(setf sprite-coord (cadr tile-pixel-coord-list))
-	(gl:bind-texture :texture-2d (gethash char texture-table))))
+	(setf path (gethash char path-table))
+	(setf sprite-coord (gethash char tile-pixel-coord-table))))
   (unless (eq path nil)
-    (mariko:draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
-		  (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift)))
+    (gl:bind-texture :texture-2d (gethash char texture-table))
+	  (mariko:draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
+		       (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift)))
 
 
-(defun draw-tiles (texture-table data-file path-list tile-pixel-coord-list x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
+(defun draw-tiles (texture-table data-file path-table tile-pixel-coord-table x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
   "read map data file and draw tiles accordingly. offset shifts the each tile in the x direction use this if tiles are not hex."
   (let ((map-list (read-file-to-list data-file)))
     (loop for char in map-list
@@ -52,13 +47,20 @@ if doing tile-map with draw-tile-map"
        and
        do (setf xshift (* count offset))
        else do (setf xshift (+ xshift x-distance-apart))
-       do (tile-selector char path sprite-coord tile-pixel-coord-list path-list
+       do (tile-selector char path sprite-coord tile-pixel-coord-table path-table
 			 texture-table xshift yshift))))	
 	 
+(defun set-tables (path-table path-list tile-pixel-coord-table tile-pixel-coord-list texture-table)
+  (setf (gethash #\0 path-table) (car path-list))
+  (setf (gethash #\1 path-table) (cadr path-list))
+  (setf (gethash #\0 tile-pixel-coord-table) (car tile-pixel-coord-list))
+  (setf (gethash #\1 tile-pixel-coord-table) (cadr tile-pixel-coord-list))
+  (setf (gethash #\0 texture-table) (mariko:load-texture (car path-list)))
+  (setf (gethash #\1 texture-table) (mariko:load-texture (cadr path-list))))
 
-(defun map-tiles (path-list coord-list texture-table)
+(defun map-tiles (path-table tile-pixel-coord-table texture-table)
   (gl:clear :color-buffer-bit)
-  (draw-tiles texture-table "sample-map.txt" path-list coord-list 65 50 :offset 30))
+  (draw-tiles texture-table "sample-map.txt" path-table tile-pixel-coord-table 65 50 :offset 30))
 			     
 (defun main ()
   (sdl2-image:init '(:png))
@@ -66,16 +68,17 @@ if doing tile-map with draw-tile-map"
     (let* ((grass "tileGrass.png")
 	   (dirt "tileDirt_full.png")
 	   (path-list (list grass dirt))
+	   (path-table (make-hash-table))
 	   (texture-table (make-hash-table))
-	   (tile-pixel-coord-list (make-sprite-list-from-singles path-list)))
-      (setf (gethash #\0 texture-table) (mariko:load-texture grass))
-      (setf (gethash #\1 texture-table) (mariko:load-texture dirt))
+	   (tile-pixel-coord-list (make-sprite-list-from-singles path-list))
+	   (tile-pixel-coord-table (make-hash-table)))
+      (set-tables path-table path-list tile-pixel-coord-table tile-pixel-coord-list texture-table)
       (glfw:set-window-size-callback 'update-viewport)
       (glfw:set-key-callback 'quit-on-escape)
       (mariko:set-viewport 800 400)
       (gl:clear-color 1 1 1 1)
       (gl:clear :color-buffer)
       (loop until (glfw:window-should-close-p)
-	 do (map-tiles path-list tile-pixel-coord-list texture-table)
+	 do (map-tiles path-table tile-pixel-coord-table texture-table)
 	 do (glfw:poll-events)
 	 do (glfw:swap-buffers)))))
