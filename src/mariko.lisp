4b22123dcdc742bd153a594f9ba7023631042110
IsemanTech <johnsoni1440@gmail.com>
2017-05-31 09:56:00 -0400

    if char = #\Tab then it will not be drawn. read-file-to-list is now its own function outside of the draw-tiles function

--- src/mariko.lisp
+++ src/mariko.lisp
@@ -149,18 +149,16 @@ if doing tile-map with draw-tile-map"
 		      texture-table xshift yshift)
   "Select tile in according to matching char key in hash tables. The chars read
 from file must have an existing hash key. If not then there will be undesired behavior."
-  (unless (or (eq char #\NewLine) (eq char #\Space))
+  (unless (or (eq char #\NewLine) (eq char #\Space) (eq char #\Tab))
       (progn
 	(setf path (gethash char path-table))
-	(setf sprite-coord (gethash char tile-pixel-coord-table))))
-  (unless (eq path nil)
-    (gl:bind-texture :texture-2d (gethash char texture-table))
-	  (draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
-		       (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift)))
+	(setf sprite-coord (gethash char tile-pixel-coord-table))
+	(gl:bind-texture :texture-2d (gethash char texture-table))
+	(draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
+		       (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift))))
 
-(defun draw-tiles (texture-table data-file path-table tile-pixel-coord-table x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
+(defun draw-tiles (map-list texture-table path-table tile-pixel-coord-table x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
   "read map data file and draw tiles accordingly. offset shifts the each tile in the x direction use this if tiles are not hex."
-  (let ((map-list (read-file-to-list data-file)))
     (loop for char in map-list
        with path 
        with sprite-coord
@@ -173,4 +171,4 @@ from file must have an existing hash key. If not then there will be undesired be
        do (setf xshift (* count offset))
        else do (setf xshift (+ xshift x-distance-apart))
        do (tile-selector char path sprite-coord tile-pixel-coord-table path-table
-			 texture-table xshift yshift))))
+			 texture-table xshift yshift)))
