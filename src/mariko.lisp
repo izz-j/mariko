cd9a85a946900b22bf61a49f8f2f2c7069bbd129
IsemanTech <johnsoni1440@gmail.com>
2017-06-04 07:39:26 -0400

    draw-tiles now has even/odd offset pattern function. This should work great for flat top hexes. Also the defualt orientation no longer moves diagonal. map-list is included in the draw-tiles function again

--- src/mariko.lisp
+++ src/mariko.lisp
@@ -146,29 +146,48 @@ if doing tile-map with draw-tile-map"
        nconc (list char))))
 
 (defun tile-selector (char path sprite-coord tile-pixel-coord-table path-table
-		      texture-table xshift yshift)
+		      texture-table xshift yshift count offset y-odd-column-offset y-even-column-offset)
   "Select tile in according to matching char key in hash tables. The chars read
-from file must have an existing hash key. If not then there will be undesired behavior."
+from file must have an existing hash key. If not then there will be undesired behavior. only one offset can be chosen at a time"
   (unless (or (eq char #\NewLine) (eq char #\Space) (eq char #\Tab))
-      (progn
+    (progn
 	(setf path (gethash char path-table))
 	(setf sprite-coord (gethash char tile-pixel-coord-table))
 	(gl:bind-texture :texture-2d (gethash char texture-table))
+	(when (and (oddp count) (eq y-odd-column-offset t))
+	  (setf yshift (+ yshift offset)))
+	(when (and (evenp count) (eq y-even-column-offset t))
+	  (setf yshift (+ yshift offset)))
 	(draw (car sprite-coord) (cadr sprite-coord) (caddr sprite-coord)
 		       (cadddr sprite-coord) (mariko:get-image-width path) (mariko:get-image-height path) :xshift xshift :yshift yshift))))
 
-(defun draw-tiles (map-list texture-table path-table tile-pixel-coord-table x-distance-apart y-distance-apart &key (offset 0) (xshift 0) (yshift 0))
-  "read map data file and draw tiles accordingly. offset shifts the each tile in the x direction use this if tiles are not hex."
+(defun get-map-width (map-list)
+  (loop for i in map-list
+     count i
+     until (eq i '#\NewLine)))
+
+
+(defun get-map-length (area width)
+    (if (= width 0)
+	(princ "Can't divide by zero")
+	(/ area width)))
+;;length and width may be used later for grid
+(defun draw-tiles (data-file texture-table path-table tile-pixel-coord-table x-distance-apart y-distance-apart &key (y-odd-column-offset nil) (y-even-column-offset nil) (offset 0) (xshift 0) (yshift 0))
+  "read map data file and draw tiles accordingly. if offset must have a value to to use and even or odd column offset"
+  (let* ((map-list (read-file-to-list data-file))
+	 (width (get-map-width map-list))
+	 (length (get-map-length (length map-list) width)))
     (loop for char in map-list
        with path 
        with sprite-coord
        with count = 0
-       if (eq char #\NewLine)
+       do (tile-selector char path sprite-coord tile-pixel-coord-table path-table texture-table xshift yshift count offset y-odd-column-offset y-even-column-offset)
+       do (setf xshift (+ xshift x-distance-apart))
+       unless (eq char #\NewLine)
        do (setf count (+ count 1))
+       if (eq char #\NewLine)
+       do (setf count 0)
        and
        do (setf yshift (+ yshift y-distance-apart))
        and
-       do (setf xshift (* count offset))
-       else do (setf xshift (+ xshift x-distance-apart))
-       do (tile-selector char path sprite-coord tile-pixel-coord-table path-table
-			 texture-table xshift yshift)))
+       do (setf xshift 0))))
